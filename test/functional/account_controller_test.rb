d3ed09d2d2c32be131431be3230828f682afef03
Gregor Schmidt <ruby@schmidtwisser.de>
2012-07-12 15:09:24 +0200

    First shot at tests - not working completely yet

---
+++ test/functional/account_controller_test.rb
@@ -0,0 +1,61 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AccountControllerTest < ActionController::TestCase
+  # Avoid bugs based on Setting cache
+  old_cache_value = nil
+
+  setup do
+    old_cache_value = Setting.use_caching?
+    Setting.use_caching = false
+  end
+
+  teardown do
+    Setting.use_caching = old_cache_value
+  end
+
+  context "with legacy password" do
+    setup do
+      Setting.plugin_safe_password_hashes = {
+        :hash_function => nil, # legacy
+        :iterations    => nil,
+        :key_length    => "40"
+      }
+
+      @user = User.generate(:login => 'alfred', :password => 'bruce')
+    end
+
+    should 'be able to log in with proper credentials' do
+      post :login, :username => 'alfred', :password => 'bruce'
+      assert_redirected_to :controller => 'my', :action => 'page'
+      assert_equal User.current, @user
+    end
+
+    should 'not be able to log in with wrong credentials' do
+      post :login, :username => 'alfred', :password => 'wayne'
+      assert_response :success
+      assert User.current.anonymous?
+    end
+
+    context "after hash_function was changed" do
+      setup do
+        Setting.plugin_safe_password_hashes = {
+          :hash_function => "pbkdf2_sha256",
+          :iterations    => "500",
+          :key_length    => "40"
+        }
+      end
+
+      should 'be able to log in with proper credentials' do
+        post :login, :username => 'alfred', :password => 'bruce'
+        assert_redirected_to :controller => 'my', :action => 'page'
+        assert_equal User.current, @user
+      end
+
+      should 'not be able to log in with wrong credentials' do
+        post :login, :username => 'alfred', :password => 'wayne'
+        assert_response :success
+        assert User.current.anonymous?
+      end
+    end
+  end
+end
---
+++ test/functional/my_controller_test.rb
@@ -0,0 +1,64 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class MyControllerTest < ActionController::TestCase
+  # Avoid bugs based on Setting cache
+  old_cache_value = nil
+
+  setup do
+    old_cache_value = Setting.use_caching?
+    Setting.use_caching = false
+  end
+
+  teardown do
+    Setting.use_caching = old_cache_value
+  end
+
+  context "with legacy password" do
+    setup do
+      Setting.plugin_safe_password_hashes = {
+        :hash_function => nil, # legacy
+        :iterations    => nil,
+        :key_length    => "40"
+      }
+
+      @user = User.generate(:login => 'alfred', :password => 'bruce')
+      @request.session[:user_id] = @user.id
+    end
+
+    should 'be able to change password without hash_function change' do
+      post :password, :password => 'bruce',
+                      :new_password => 'dick',
+                      :new_password_confirmation => 'dick'
+
+      @user.reload
+
+      assert_redirected_to :controller => 'my', :action => 'account'
+      assert_equal User.current, @user
+      assert_nil @user.password_hash_function
+      assert_equal @user.hash_with_legacy('dick'), @user.hashed_password
+    end
+
+    context "after hash_function was changed" do
+      setup do
+        Setting.plugin_safe_password_hashes = {
+          :hash_function => "pbkdf2_sha256",
+          :iterations    => "500",
+          :key_length    => "40"
+        }
+      end
+
+      should 'be able to change password with hash_function change' do
+        post :password, :password => 'bruce',
+                        :new_password => 'dick',
+                        :new_password_confirmation => 'dick'
+
+        @user.reload
+
+        assert_redirected_to :controller => 'my', :action => 'account'
+        assert_equal User.current, @user
+        assert_equal 'pbkdf2_sha256', @user.password_hash_function
+        assert_equal @user.hash_with_pbkdf2('dick'), @user.hashed_password
+      end
+    end
+  end
+end
